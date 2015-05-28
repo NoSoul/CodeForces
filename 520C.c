3304e1acdd0a64da9d01609edb90580195ea195c
NoSoul <NoSoul.Love@GMail.com>
2015-05-28 18:37:52 +0800

    2015/05/28

---
+++ 520C.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+
+int ExpMod(int a, int n, int mod)
+{
+    int ans = 1;
+    a %= mod;
+    while(n)
+    {
+        if(n & 1)
+        {
+            ans = ((long long)ans  * a) % mod;
+        }
+        a = ((long long)a * a) % mod;
+        n >>= 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    int N, i, j;
+    int A[4];
+    char Str[100001];
+    memset(A, 0, sizeof(A));
+    scanf("%d %s", &N, Str);
+    for(i = 0; Str[i] != '\0'; ++i)
+    {
+        if(Str[i] == 'A')
+        {
+            ++A[0];
+        }
+        else if(Str[i] == 'C')
+        {
+            ++A[1];
+        }
+        else if(Str[i] == 'G')
+        {
+            ++A[2];
+        }
+        else if(Str[i] == 'T')
+        {
+            ++A[3];
+        }
+    }
+    int idx = 0, Cnt = 1;
+    for(i = 1; i < 4; ++i)
+    {
+        if(A[i] > A[idx])
+        {
+            idx = i;
+            Cnt = 1;
+        }
+        else if(A[i] == A[idx])
+        {
+            ++Cnt;
+        }
+    }
+    printf("%d\n", ExpMod(Cnt, N, 1000000000 + 7));
+    return 0;
+}
---
+++ 548A.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int Palindrome(char *str, int s, int e)
+{
+    while(s < e)
+    {
+        if(str[s] != str[e])
+        {
+            return 0;
+        }
+        ++s;
+        --e;
+    }
+    return 1;
+}
+
+int main()
+{
+    char Str[1001];
+    int K, L, i, M;
+    scanf("%s %d", Str, &K);
+    L = strlen(Str);
+    if(L % K)
+    {
+        puts("NO");
+        return 0;
+    }
+    M = L / K;
+    for(i = 0; i < L; i += M)
+    {
+        if(!Palindrome(Str, i, i + M - 1))
+        {
+            puts("NO");
+            return 0;
+        }
+    }
+    puts("YES");
+    return 0;
+}
